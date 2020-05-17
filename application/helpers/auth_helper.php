f4acf7a770107cd195c98ed1f3c76876973f9fef
Bima Febriansyah <bimafebriansyah1002@gmail.com>
2020-05-17 10:15:17 +0700

    change is_logged_in

--- application/helpers/auth_helper.php
+++ application/helpers/auth_helper.php
@@ -1,11 +1,13 @@
 <?php
 
-function is_logged_in($is_auth)
+function is_logged_in($is_auth = false, $is_profile = false)
 {
 	$CI = &get_instance();
 	$is_login = $CI->session->userdata('is_logged');
 	if ($is_login) {
-		redirect('profile');
+		if (!$is_profile) {
+			redirect('profile');
+		}
 	} else {
 		if (!$is_auth) {
 			$CI->session->set_userdata(['current_url' => current_url()]);
