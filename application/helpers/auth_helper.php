a7792a212949bd6b5a3951d24a504e9d9fe18f9e
Bima Febriansyah <bimafebriansyah1002@gmail.com>
2020-05-23 16:26:46 +0700

    modified auth helper

--- application/helpers/auth_helper.php
+++ application/helpers/auth_helper.php
@@ -1,27 +1,44 @@
 <?php
 
-function is_logged_in($is_auth = false, $is_profile = false)
+function is_login()
 {
 	$CI = &get_instance();
 	$is_login = $CI->session->userdata('is_logged');
-	if ($is_login) {
-		if (!$is_profile) {
-			redirect('profile');
-		}
-	} else {
-		if (!$is_auth) {
-			$CI->session->set_userdata(['current_url' => current_url()]);
-			redirect('auth');
-		}
+	if (!$is_login) {
+		$CI->session->set_flashdata('message', "<script>$(document).ready(function() { toastr.error('Please login to continue');});</script>");
+		redirect('/');
 	}
 }
 
 function is_user()
 {
-	$is_user = $this->session->userdata('role');
+	is_login();
+	$CI = &get_instance();
+	$is_user = $CI->session->userdata('role');
+	if ($is_user != 1) {
+		$CI->session->set_flashdata('message', "<script>$(document).ready(function() { toastr.warning('You dont have access');});</script>");
+		redirect('/');
+	}
 }
 
 function is_admin()
 {
-	$is_admin = $this->session->userdata('role');
+	is_login();
+	$CI = &get_instance();
+	$is_admin = $CI->session->userdata('role');
+	if ($is_admin != 2 && $is_admin != 3) {
+		$CI->session->set_flashdata('message', "<script>$(document).ready(function() { toastr.warning('You dont have access');});</script>");
+		redirect('/');
+	}
+}
+
+function is_staff()
+{
+	is_login();
+	$CI = &get_instance();
+	$is_staff = $CI->session->userdata('role');
+	if ($is_staff != 4) {
+		$CI->session->set_flashdata('message', "<script>$(document).ready(function() { toastr.warning('You dont have access');});</script>");
+		redirect('/');
+	}
 }
