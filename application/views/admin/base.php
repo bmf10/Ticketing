040af3273bd016c4fbdc3e83b386f0e6608b0e15
Bima Febriansyah <bimafebriansyah1002@gmail.com>
2020-05-31 15:40:17 +0700

    add userlogin

--- application/views/admin/base.php
+++ application/views/admin/base.php
@@ -1,3 +1,7 @@
+<?php
+$user_login = $this->db->get_where('users', ['id' => $this->session->userdata('id')])->row();
+?>
+
 <!DOCTYPE html>
 <!--
 This is a starter template page. Use this page to start your new project from
@@ -81,10 +85,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
 			<div class="sidebar">
 				<div class="user-panel mt-3 pb-3 mb-3 d-flex">
 					<div class="image">
-						<img src="<?= base_url('assets/admin/') ?>dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+						<img src="<?= base_url() ?>assets/assets/img/default.png" class="img-circle elevation-2" alt="User Image">
 					</div>
 					<div class="info">
-						<a href="#" class="d-block">Alexander Pierce</a>
+						<a href="#" class="d-block"><?= $user_login->name ?></a>
 					</div>
 				</div>
 
--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,9 +1,51 @@
 <div class="row">
 	<div class="col-md-12">
-		<a href="<?= base_url('admin/dashboard/admin') ?>" class="btn btn-primary my-2">New Admin</a>
+		<?php if ($this->session->userdata('role') == 3) : ?>
+			<a href="<?= base_url('admin/dashboard/admin') ?>" class="btn btn-primary my-2">New Admin</a>
+		<?php endif ?>
+		<a href="<?= base_url('admin/dashboard/profile') ?>" class="btn btn-primary my-2">Edit Profile</a>
 		<div class="card">
 			<div class="card-body">
-				Hello
+				<div class="row">
+					<div class="col-md-4">
+						<h4 class="text-center">Your Profile</h4>
+						<div class="text-center">
+							<img src="<?= base_url() ?>assets/assets/img/default.png" class="img-thumbnail" /><br><br>
+							<p style="margin-bottom: -3px; font-weight: bold">Name</p>
+							<p style="margin-bottom: 5px;"><?= $user->name ?></p>
+							<p style="margin-bottom: -3px; font-weight: bold">Gender</p>
+							<p style="margin-bottom: 5px;"><?= $user->gender ?></p>
+							<p style="margin-bottom: -3px; font-weight: bold">Address</p>
+							<p style="margin-bottom: 5px;"><?= $user->address ?></p>
+							<p style="margin-bottom: -3px; font-weight: bold">Phone</p>
+							<p style="margin-bottom: 5px;"><?= $user->phone ?></p>
+							<p style="margin-bottom: -3px; font-weight: bold">Email</p>
+							<p style="margin-bottom: 5px;"><?= $user->email ?></p>
+						</div>
+					</div>
+					<div class="col-md-1">
+						<div style="width: 1px; height: 100%; background-color: grey"></div>
+					</div>
+					<div class="col-md-7">
+						<h4 class="text-center">Last Transaction</h4>
+						<table class="table table-striped table-hover">
+							<thead>
+								<th>ID</th>
+								<th>User</th>
+								<th>Date</th>
+								<th>Status</th>
+							</thead>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>User 1</td>
+									<td>10/10/1910</td>
+									<td>Waiting for payment</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
